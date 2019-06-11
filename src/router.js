e3f6eb9b452a6907487027457565e64f02f4c347
lavendeyan <247729331@qq.com>
2019-06-11 17:02:05 +0800

    许文举_第二次_CustomerPage

--- src/router.js
+++ src/router.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+// import OrderLinePage from './routes/OrderLinePage'
+import CustomerPage from './routes/CustomerPage'
+// import OrderPage from './routes/OrderPage'
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+{/* <<<<<<< HEAD */}
+        <Route path="/customer/findAllCustomer" exact component={CustomerPage} />
+{/* ======= */}
+        {/* <Route path="/orderLine" exact component={OrderLinePage} /> */}
+        <Route path="/customer" exact component={CustomerPage} />
+        {/* <Route path="/order" exact component={OrderPage} /> */}
+{/* >>>>>>> 12c974fe0c9f0aeca9b2d9d5a41847b3c4248a08 */}
       </Switch>
     </Router>
   );
