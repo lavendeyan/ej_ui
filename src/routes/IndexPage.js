6008d1d850abdc6a863a365d02739dcdfdc47f78
troye12345 <1091325506@qq.com>
2019-06-14 17:54:25 +0800

    张猛第7次上传主页修改

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -85,7 +85,17 @@ class IndexPage extends React.Component {
         </Sider>
         <Layout>
           <Content>
-            <div style={{  background: '#f6f9fa', minHeight: 300 }}>{this.props.children ? this.props.children : <h1 className={styles.title}>e洁</h1>}</div>
+            <div>
+               {/* style={{  background: '#f6f9fa', minHeight: 300 }}>{this.props.children ? this.props.children : <h1 className={styles.title}>Yay! Welcome to dva!</h1> */}
+                  <div className={styles.normal}>
+                  <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+                  <div className={styles.welcome} />
+                  <ul className={styles.list}>
+                  <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+                  <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+                  </ul>
+                </div>
+              </div>
           </Content>
         </Layout>
       </Layout>
